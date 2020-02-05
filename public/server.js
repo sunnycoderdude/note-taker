8c4d2cbd2392d54815b0a06f535dd9bdc659ed83
Satinder Singh <spalsingh91@gmail.com>
2020-02-05 17:30:02 -0500

    fixing path

--- public/server.js
+++ public/server.js
@@ -18,11 +18,12 @@ app.use(express.json());
 
 // Basic route that sends the user first to the AJAX Page
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
   });
   
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    console.log(__dirname);
 });
 
 // Starts the server to begin listening

bfce62b7b99b2df695bf55a4c1df872efbe5255d
Satinder Singh <spalsingh91@gmail.com>
2020-02-05 17:13:13 -0500

    adding html routes

---
+++ server.js
@@ -0,0 +1,32 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const path = require("path");
+const fs = require ("fs"); 
+const util = require ("util");
+const writeFileAsync = util.promisify(fs.writeFile); 
+const readFileAsync = util.promisify(fs.readFile);
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Basic route that sends the user first to the AJAX Page
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+  
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
